71d8fdd18815db2ad3190f6b62657287a71b9c05
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-07-30 21:53:12 +0200

    try to use custom asset path
    https://github.com/lukeed/sirv/issues/9#issuecomment-408708177

--- app/server.js
+++ app/server.js
@@ -4,10 +4,15 @@ import sapper from 'sapper';
 import compression from 'compression';
 import { manifest } from './manifest/server.js';
 
+const assets = sirv('assets');
+
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('assets'),
+		(req, res, next) => {
+			// check if we're looking at a /static/* request
+			return req.path.startsWith('/assets/') ? assets(req, res, next) : next();
+		},
 		sapper({ manifest })
 	)
 	.listen(process.env.PORT)
